54454e84e7345baf8a25e09050c7c0cd934b22e9
hustccc <1276675421@qq.com>
2021-01-13 05:37:51 +0900

    first commit

--- xscommand/target/debug/deps/xscommand-9a4eaf400ae02451.d
+++
@@ -1,5 +0,0 @@
-/home/hustccc/rust-xs-test/xscommand/target/debug/deps/xscommand-9a4eaf400ae02451.rmeta: src/lib.rs
-
-/home/hustccc/rust-xs-test/xscommand/target/debug/deps/xscommand-9a4eaf400ae02451.d: src/lib.rs
-
-src/lib.rs:
--- xscommand/target/debug/deps/xscommand-a7f6bb3f64d59b6b.d
+++
@@ -1,5 +0,0 @@
-/home/hustccc/rust-xs-test/xscommand/target/debug/deps/xscommand-a7f6bb3f64d59b6b.rmeta: src/lib.rs
-
-/home/hustccc/rust-xs-test/xscommand/target/debug/deps/xscommand-a7f6bb3f64d59b6b.d: src/lib.rs
-
-src/lib.rs:
