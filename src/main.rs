3289fdec45bee2606c8558356a4622b71e8831e8
hustccc <1276675421@qq.com>
2021-01-18 23:26:37 +0900

    delete some comment and add fix ram.h

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ extern crate chrono;
 extern crate psutil;
 
 #[allow(unused_imports)]
-use std::{fs, path::Path, process::{Command, exit}, thread, time::Duration, vec};
+use std::{
+    fs,
+    path::Path,process::{Command, exit},
+    thread, time::Duration,
+    vec,
+    io::{prelude::*, BufWriter},
+};
 use simple_logger::SimpleLogger;
 #[allow(unused_imports)]
 use xscommand::{
@@ -74,37 +80,6 @@ fn main() -> ! {
                     exit(-1);
                 }
             }
-            // {
-            //     let stdout_f = workload.join("stdout.txt");
-            //     let stderr_f = workload.join("stderr.txt");
-            //     let mut git = Git::new("git");
-            //     let args = vec!["clone", repo_url];
-            //     // use macro to reduce below code lines
-            //     git.set_args(args);
-            //     match git.set_workdir(workload.to_str()) {
-            //         Ok(_) => {}, // do nothing
-            //         Err(git_err) => {
-            //             log::error!("{}, thread {} exit.", git_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            //     match git.excute(stdout_f.to_str(), stderr_f.to_str()) {
-            //         Ok(exit_code) => {
-            //             log::info!("git with args: {:?} excute return {}", git.get_args(), exit_code);
-            //             if exit_code != 0 {
-            //                 log::error!("exit not zero, thread {} exit.", thread_id::get());
-            //                 exit(-1);
-            //             }
-            //         },
-            //         Err(git_err) => {
-            //             log::error!("{}, thread {} exit.", git_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            //     // git drop here
-            // }
             let workload = workload.join(url2path(repo_url));
             // enter XiangShan and make init
             match Make::init(workload.to_str()) {
@@ -120,35 +95,50 @@ fn main() -> ! {
                     exit(-1);
                 }
             }
-            // {
-            //     let stdout_f = workload.join("stdout.txt");
-            //     let stderr_f = workload.join("stderr.txt");
-            //     let mut make = Make::new("make");
-            //     make.set_args(vec!["init"]);
-            //     match make.set_workdir(workload.to_str()) {
-            //         Ok(_) => {}, // do nothing
-            //         Err(make_err) => {
-            //             log::error!("{}, thread {} exit", make_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            //     match make.excute(stdout_f.to_str(), stderr_f.to_str()) {
-            //         Ok(exit_code) => {
-            //             log::info!("make with args: {:?} excute return {}", make.get_args(), exit_code);
-            //             if exit_code != 0 {
-            //                 log::error!("exit not zero, thread {} exit.", thread_id::get());
-            //                 exit(-1);
-            //             }
-            //         },
-            //         Err(make_err) => {
-            //             log::error!("{}, thread {} exit.", make_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            //     // make drop here
-            // }
+            // change the ram size
+            let ram_h = workload.join("src/test/csrc/ram.h");
+            let ram_h_contents = match fs::read_to_string(&ram_h) {
+                Ok(content) => content,
+                Err(_) => {
+                    log::error!("failed to read ram.h, thread {} exit.", thread_id::get());
+                    exit(-1);
+                }
+            };
+            let new_contents: Vec<&str> = ram_h_contents.lines().map(|f| {
+                if f.contains("#define EMU_RAM_SIZE (256 * 1024 * 1024UL)") {
+                    "// #define EMU_RAM_SIZE (256 * 1024 * 1024UL)"
+                } else if f.contains("// #define EMU_RAM_SIZE (8 * 1024 * 1024 * 1024UL)") {
+                    "#define EMU_RAM_SIZE (8 * 1024 * 1024 * 1024UL)"
+                } else {
+                    f
+                }
+            }).collect();
+            {
+                let f = match fs::File::create(ram_h) {
+                    Ok(ram_f) => ram_f,
+                    Err(_) => {
+                        log::error!("failed to open ram.h, thread {} exit", thread_id::get());
+                        exit(-1);
+                    }
+                };
+                let mut buf_writer = BufWriter::new(f);
+                for line in new_contents {
+                    match buf_writer.write(line.as_bytes()) {
+                        Ok(_) => {},
+                        Err(_) => {
+                            log::error!("BufWriter write line error, thread {} exit.", thread_id::get());
+                            exit(-1);
+                        }
+                    }
+                    match buf_writer.write(b"\n") {
+                        Ok(_) => {},
+                        Err(_) => {
+                            log::error!("BufWriter write \\n error, thread {} exit.", thread_id::get());
+                            exit(-1);
+                        }
+                    }
+                }
+            }
             // numatcl -C 0-255 make build/emu EMU_TRACE=1 SIM_ARGS="--disable-all" EMU_THREADS=8 -j256
             match Numactl::make_emu(workload.to_str(), NEMU_HOME, AM_HOME) {
                 Ok(exit_code) => {
@@ -163,37 +153,6 @@ fn main() -> ! {
                     exit(-1);
                 }
             }
-            // {
-            //     let stdout_f = workload.join("stdout.txt");
-            //     let stderr_f = workload.join("stderr.txt");
-            //     let mut make = Make::new("numactl");
-            //     make.set_args(vec!["-C", "0-255", "make", "build/emu", "EMU_TARCE=1", "SIM_ARGS=\"--disable-all\"", "EMU_THREADS=8", "-j256"]);
-            //     match make.set_workdir(workload.to_str()) {
-            //         Ok(_) => {}, // do nothing
-            //         Err(make_err) => {
-            //             log::error!("{}, thread {} exit", make_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            //     make.exe.env("NEMU_HOME", NEMU_HOME);
-            //     make.exe.env("AM_HOME", AM_HOME);
-            //     match make.excute(stdout_f.to_str(), stderr_f.to_str()) {
-            //         Ok(exit_code) => {
-            //             log::info!("make with args: {:?} excute return {}", make.get_args(), exit_code);
-            //             if exit_code != 0 {
-            //                 log::error!("exit not zero, thread {} exit.", thread_id::get());
-            //                 exit(-1);
-            //             }
-            //         },
-            //         Err(make_err) => {
-            //             log::error!("{}, thread {} exit.", make_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            //     // make drop here
-            // }
             // create ../emu_res dir && 
             // numactl -C [] emu -I 1000000 -i /bigdata/zyy/checkpoints_profiles/betapoint_profile_06/gcc_200/0/_8000000000_.gz
             let res_dir = workload.join("../emu_res");
@@ -239,103 +198,6 @@ fn main() -> ! {
                         exit(-1);
                     }
                 }
-            // {
-            //     if !res_dir.exists() {
-            //         match fs::create_dir_all(res_dir.as_path()) {
-            //             Ok(_) => {}, // do nothing
-            //             Err(msg) => {
-            //                 log::error!("Failed in creating res_dir {:?} with msg
-            //                 {} , thread {} exit.", res_dir, msg, thread_id::get());
-            //                 // TODO: specify exit code
-            //                 exit(-1);
-            //             },
-            //         }
-            //     }
-            //     let stdout_f = res_dir.join("stdout.txt");
-            //     let stderr_f = res_dir.join("stderr.txt");
-            //     let emu_path = workload.join("./build/emu");
-            //     let emu = if let Some(path) = emu_path.to_str() {
-            //         log::info!("create emu in {}", path);
-            //         path
-            //     } else {
-            //         log::error!("no path in emu_path, thread {} exit", thread_id::get());
-            //         exit(-1);
-            //     };
-            //     let mut cpu_percent_collector = match psutil::cpu::CpuPercentCollector::new() {
-            //         Ok(collector) => collector,
-            //         Err(_) => {
-            //             log::error!("new CpuPercentCollector error, thread {} exit.", thread_id::get());
-            //             exit(-1);
-            //         }
-            //     };
-            //     let mut numactl = Numactl::new("numactl");
-            //     let cpu_percents = match cpu_percent_collector.cpu_percent_percpu() {
-            //         Ok(percents) => percents,
-            //         Err(_) => {
-            //             log::error!("get cpu_percent_percpu error, thread {} exit.", thread_id::get());
-            //             exit(-1);
-            //         }
-            //     };
-            //     let (mut begin, mut end) = (0, 0);
-            //     let mut count = 0;
-            //     for _ in 0..3 {
-            //         begin = 0;
-            //         end = 0;
-            //         for i in 0..cpu_percents.len() {
-            //             if cpu_percents[i] > 50.0 {
-            //                 begin += 1;
-            //                 end = begin;
-            //             } else {
-            //                 end += 1;
-            //             }
-            //             if (end - begin) >= 7 {
-            //                 // found 8 free cpus, break
-            //                 break;
-            //             }
-            //         }
-            //         if (end - begin) >= 7 {
-            //             log::info!("found avaiable 8 cpus, ready to run emu.");
-            //             break;
-            //         }
-            //         count += 1;
-            //         thread::sleep(Duration::from_secs(SLEEP_TIME * 100));
-            //     }
-            //     if count >= 3 {
-            //         log::error!("no 8 cpus avaiable, thread {} exit.", thread_id::get());
-            //         exit(-1);
-            //     }
-            //     let mut cpu_ids = begin.to_string();
-            //     cpu_ids.push_str("-");
-            //     cpu_ids.push_str(end.to_string().as_str());
-            //     log::info!("avaiable cpus: {}", cpu_ids);
-            //     numactl.set_args(vec!["-C", cpu_ids.as_str(), emu, "-I", "1000000", "-i", EMU_TARGET]);
-            //     match numactl.set_workdir(workload.to_str()) {
-            //         Ok(_) => {}, // do nothing
-            //         Err(emu_err) => {
-            //             log::error!("{}, thread {} exit", emu_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            //     // set env `NOOP_HOME` to currnet workload
-            //     numactl.exe.env("NOOP_HOME", workload.to_path_buf());
-            //     numactl.exe.env("NEMU_HOME", NEMU_HOME);
-            //     numactl.exe.env("AM_HOME", AM_HOME);
-            //     match numactl.excute(stdout_f.to_str(), stderr_f.to_str()) {
-            //         Ok(exit_code) => {
-            //             log::info!("make with args: {:?} excute return {}", numactl.get_args(), exit_code);
-            //             if exit_code != 0 {
-            //                 log::error!("exit not zero, thread {} exit.", thread_id::get());
-            //                 exit(-1);
-            //             }
-            //         },
-            //         Err(emu_err) => {
-            //             log::error!("{}, thread {} exit.", emu_err.as_str(), thread_id::get());
-            //             // TODO: specify exit code
-            //             exit(-1);
-            //         }
-            //     }
-            // }
             log::info!("thread {} return 0", thread_id::get());
             
         });
